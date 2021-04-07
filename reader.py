c768ec45d1c601adbea0474a8f75580d0ff9810c
Alejandro Tejada <tej17584@uvg.edu.gt>
2021-04-07 16:21:00 -0600

    Files de ejemplo para lector

---
+++ reader.py
@@ -0,0 +1,8 @@
+"""
+Nombre: Alejandro Tejada
+Curso: Diseño lenguajes de programacion
+Fecha: Abril 2021
+Programa: reader.py
+Propósito: Este programa tiene como propósito leer el archivo con extension .ATG  
+V 1.0
+"""
