5e2e5d33130abaea86aa8092978ade7b4aebde43
Alejandro Tejada <tej17584@uvg.edu.gt>
2021-04-26 13:24:20 -0600

    Editando el posftix para que sea bueno
    El postfix ahora es el del proyecto #1

--- postFixTokens.py
+++ postFixTokens.py
@@ -12,21 +12,22 @@ from collections import OrderedDict
 from funciones import *
 
 
-class Conversion:
+class ConversionPostfix:
     # Constructor de las variables
     def __init__(self):
-        self.top = -1  # ! límite
+        self.top = -1
         # self.capacity = capacity  # capacidad o longitud de la cadena
         # El array se usa como un stack
         self.arrayOperandos = []
         # seteamos la precedencia. La suma y resta es 1, la multiplicacion y division son 2 y la exponenciacion es 3, siguiendo reglas de la aritmética
         self.outputPostfix = []
-        self.precedence = {'+': 1, '-': 1}  # diccionario de precedencia
+        self.precedenceV2 = {'|': 1, '.': 2,
+                             '*': 3, '?': 3, "+": 3}  # diccionario de precedencia version 2
         self.concatenado = ''
+        self.validacion = False
         self.funciones = funciones()
 
     # método para ver si el stack esta vacío
-
     def isEmpty(self):
         return True if self.top == -1 else False
 
@@ -42,33 +43,33 @@ class Conversion:
         else:
             return "$"  # de lo contrario un valor espaecial
 
+    # Se verifica si la precedencia de un operandor es estrictamente menor que la del primer elemento del stack
+    def mayorPrecedencia(self, i):
+        try:
+            a = self.precedenceV2[i]
+            b = self.precedenceV2[self.peekTopOfStack()]
+            return True if a <= b else False
+        except KeyError:
+            return False
+
     # hace push de un elemento
     def push(self, op):
         self.top += 1  # sumamos al top, en este caso, si es el primer elemento o unico, será 0 porque contiene un -1 por defecto
         self.arrayOperandos.append(op)  # se hace pop
 
-    # Se verifica si la precedencia de un operandor es estrictamente menor que la del primer elemento del stack
-    def mayorPrecedencia(self, i):
-        try:
-            a = self.precedence[i]
-            b = self.precedence[self.peekTopOfStack()]
-            return True if a <= b else False
-        except KeyError:
-            return False
-
-    # Main, convierte infix a postfix
+    # ? Método principal para convertir
     def infixToPostfix(self, exp):
         # Iteramos sobre la exppresión
         for i in exp:
             # Si el caracter es un operando se añade al print final
-            if self.funciones.isOperand(i):
-                # self.outputPostfix.append(i)
-                self.concatenado = self.concatenado+i
-            elif self.funciones.is_op(i):
-                if(self.concatenado != ''):
-                    self.outputPostfix.append(self.concatenado)
-                    self.concatenado = ''
-                    # print(f'El valor múltiple era: {self.outputPostfix}')
+            if self.funciones.isOperandPosftixToken(i):
+                #s = self.numberToLetter(i)
+                if(i != "ε"):
+                    s = i.lower()
+                else:
+                    s = i
+                self.outputPostfix.append(s)
+            elif self.funciones.is_op_PosftixToken(i):
                 while len(self.arrayOperandos) > 0 and self.arrayOperandos[-1] != '(' and self.mayorPrecedencia(i):
                     top = self.pop()
                     self.outputPostfix.append(top)
@@ -77,9 +78,6 @@ class Conversion:
                 self.push(i)
             # Si el caracter entrante es el cierre de paréntesis, hacemos pop y lo mandamos a outputPostfix hasta que encontremos otra abertura de paréntesis
             elif i == ')':
-                if(self.concatenado != ''):
-                    self.outputPostfix.append(self.concatenado)
-                    self.concatenado = ''
                 # mientras no sea vacío y sea distinto a "("
                 while((not self.isEmpty()) and self.peekTopOfStack() != '('):
                     a = self.pop()  # hacemos pop
@@ -92,38 +90,28 @@ class Conversion:
                     return -1
                 else:
                     self.pop()  # de lo contrario, hacemos pop de valores
-            # Si encontramos un operador
-            else:
-                # mientras no sea vacío Y su precedencia sea menor a la del primer valor del stack
-                while(not self.isEmpty() and self.mayorPrecedencia(i)):
-                    # entonces agregamos a la salida el valor del stack
-                    self.outputPostfix.append(self.pop())
-                self.push(i)  # hacemos push del operando  al stack
-
-        if(self.concatenado != ''):
-            self.outputPostfix.append(self.concatenado)
-
         # HAcemos POP de TODOS los operadores en el stack
         # while not self.isEmpty():
-            # self.outputPostfix.append(self.pop())
+         #   self.outputPostfix.append(self.pop())
 
          # verificamos si existe un paréntesis abierto de más
         while len(self.arrayOperandos):
             caracter = self.pop()
             if caracter == "(":
-                print("Hay un signo de paréntesis abierto de más")
-                exit(-1)
+                return "ERRORPOSTFIX"
             self.outputPostfix.append(caracter)
 
         # Imprimimos
-        # print(" ".join(self.outputPostfix))
+        #print(" ".join(self.outputPostfix))
         return " ".join(self.outputPostfix)
 
 
+funcioncitas = funciones()
 expresion = input('Ingresa una expresión:  ')
 expresion = expresion.replace(' ', '')
-obj = Conversion()
-postFixValue = obj.infixToPostfix(expresion)
+obj = ConversionPostfix()
+expresionAlterada = funcioncitas.alterateREPosftixToken(expresion)
+postFixValue = obj.infixToPostfix(expresionAlterada)
 print(f'El resultado es: {postFixValue}')
 #strconv = postFixValue.split(' ')
 #resultado = obj.operatePostFix(strconv)
