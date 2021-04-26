2fe59e1c3b41a1cd5e74cb162407bf8fed82d6d9
Alejandro Tejada <tej17584@uvg.edu.gt>
2021-04-25 20:54:31 -0600

    cambiando path ATG

--- reader.py
+++ reader.py
@@ -22,7 +22,7 @@ class Reader:
     """
 
     def __init__(self) -> None:
-        self.rutaFile = "ATGFilesExamples\HexNumberP.ATG"
+        self.rutaFile = "ATGFilesExamples\DoubleP.ATG"
         self.streamCompleto = ""
         self.dictArchivoEntrada = ""
         self.lineasArchivo = []
