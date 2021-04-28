cbb3ac399ac7a3b4835abf2ee28544560e06a754
Alejandro Tejada <tej17584@uvg.edu.gt>
2021-04-27 18:05:58 -0600

    Funciones de mover

--- scannerTejada.py
+++ scannerTejada.py
@@ -19,7 +19,7 @@ class Scanner():
         self.abrirFiles()
 
     def abrirFiles(self):
-        # abrimos todos los files
+        # abrimos todos los files y asignamos
         infile = open("dicionarioAFDFinal", 'rb')
         self.AFDConstruidoFinal = pickle.load(infile)
         infile.close()
@@ -30,6 +30,51 @@ class Scanner():
         self.nodosAceptacion = pickle.load(infile)
         infile.close()
         print("HOKISIW")
+
+    def mover(self, estado, caracter):
+        """
+        Esta funcion retorna el siguiente estado
+        """
+        arrayEvaluar = self.AFDConstruidoFinal
+        arrayMover = []
+        for estados in estado:
+            for x in arrayEvaluar:
+                variableIn = (ord(caracter)) in x[2]
+                if(variableIn and len(x[3]) > 0 and estados == x[0]):
+                    estadoSiguiente = self.getStateNumberForArray(x[3])
+                    if(estadoSiguiente not in arrayMover):
+                        arrayMover.append(estadoSiguiente)
+
+    def getFinalStateAFN(self):
+        arrayValores = []
+        estadosFinales = self.getFinalStateNumber()
+        for valor in self.AFDConstruidoFinal:
+            for x in estadosFinales:
+                if(str(x) in valor[1]):
+                    arrayValores.append(valor[0])
+
+        arrayValores = list(
+            dict.fromkeys(arrayValores))
+        return arrayValores
+
+    def getFinalStateAFNV2(self):
+        arrayValores = []
+        estadosFinales = self.getFinalStateNumber()
+        for valor in self.AFDConstruidoFinal:
+            for x in estadosFinales:
+                if(str(x) in valor[1]):
+                    if(valor[1] not in arrayValores):
+                        arrayValores.append(valor[1])
+
+        return arrayValores
+
+    def getFinalStateNumber(self):
+        array = []
+        for numero, valor in self.diccionarioSiguientePos.items():
+            if len(valor) == 0:
+                array.append(numero)
+
+        return array
 
 
 objeSCanner = Scanner()
